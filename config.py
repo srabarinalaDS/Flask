5ddb3bbd2052b13326510f3db7c18f397a305106
srabarinalaDS <srabarinala.ds@gmail.com>
2022-12-22 14:21:39 +0100

    added data model

--- config.py
+++ config.py
@@ -3,4 +3,6 @@ class Config(object):
 class ProdConfig(Config):
     pass
 class DevConfig(Config):
-    DEBUG = True 
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:toor@localhost:3306/cardata"
+    SQLALCHEMY_ECHO = True
